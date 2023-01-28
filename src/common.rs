0a60059036a5cb4f1abaea5c18de8038d2373bdb
San <sganis@gmail.com>
2023-01-28 16:37:20 +0300

    Added common module

--- src/common.rs
+++ src/common.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct MouseEvent {
-    pub typ: &'static str,
+    pub typ: String,
     pub x: f64,
     pub y: f64,
     pub modifiers: u32,
