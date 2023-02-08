b7b24573fdf613f1c117a816bc1b59b54b0a1659
San <sganis@gmail.com>
2023-02-08 19:15:17 +0300

    Added rectangle dif

--- examples/rectangles.rs
+++ examples/rectangles.rs
@@ -1,3 +1,4 @@
+use std::path::Path;
 use image::GenericImageView;
 
 struct Rec {
@@ -13,50 +14,92 @@ pub fn vec_equal(va: &[u8], vb: &[u8]) -> bool {
     va.len() == vb.len() && 
         va.iter().zip(vb).all(|(a,b)| *a==*b)
 }
+#[cfg(target_os = "windows")]
+fn fix_path<P: AsRef<Path>>(p: P) -> String {
+    const VERBATIM_PREFIX: &str = r#"\\?\"#;
+    let p = p.as_ref().display().to_string();
+    if p.starts_with(VERBATIM_PREFIX) {
+        p[VERBATIM_PREFIX.len()..].to_string()
+    } else {
+        p
+    }
+}
 fn main() {
-    let img1 = image::open("C:\\Users\\san\\Pictures\\Screenshots\\Screenshot1.png").unwrap();
-    let img2 = image::open("C:\\Users\\san\\Pictures\\Screenshots\\Screenshot2.png").unwrap();
+    let mut dir = std::env::current_exe().unwrap();
+    dir.pop();
+    let dir = fix_path(dir);
+    println!("{}",dir);
+    let img1path = format!("{}/../../../examples/Screenshot1.png",dir);
+    println!("{}",img1path);
     
+    let img1 = image::open(&img1path).unwrap();
+    let (width, height) = img1.dimensions();
     let bytes1 = img1.to_rgb8().into_raw();
+    
+    let img2 = image::open(&format!("{}/../../../examples/Screenshot2.png",dir)).unwrap();
     let bytes2 = img2.to_rgb8().into_raw();
-    let (swidth, sheight) = img1.dimensions();
-    let swidth = swidth as u16;
-    let sheight = sheight as u16;
 
-    println!("dimensions 1 {:?}", img1.dimensions());
-    println!("dimensions 2 {:?}", img2.dimensions());
-    println!("bytes len: {}", bytes1.len());
-    
     println!("img diff: {}", vec_equal(&bytes1, &bytes2));
-    
-    let mut rectangles = Vec::<Rec>::new();
 
-    let lwidth = 200 as u16;
-    let lheight = 200 as u16;
+    let rectangles1 = get_rectangles(&bytes1, width as u16, height as u16);
+    let rectangles2 = get_rectangles(&bytes2, width as u16, height as u16);
+    
+    // sort 
+    // rectangles1.sort_by_key(|r| r.index);
+    // rectangles2.sort_by_key(|r| r.index);
+
+    let mut different_indeces = Vec::new();
+    
+    for a in 0..rectangles1.len()-1 {
+        let ra = &rectangles1[a];
+        let rb = &rectangles2[a];
+        if !vec_equal(&ra.bytes, &rb.bytes) {            
+            different_indeces.push(a);            
+        }
+    }
+    // for i in &different_indeces {
+    //     println!("{}: different", i);
+    // }
+    println!("number of rects: {}", rectangles1.len());
+    println!("different      : {}", different_indeces.len());
+    println!("percent        : {}", different_indeces.len() as f32/rectangles1.len() as f32 * 100.0);
+
+
+    // save rectangles
+    // for i in &different_indeces {
+    //     let r = &rectangles2[*i as usize];
+    //     let name = format!("temp/Result_{}.png", r.index);
+    //     image::save_buffer(&name, 
+    //         &r.bytes, r.width as u32, r.height as u32, 
+    //         image::ColorType::Rgb8).unwrap();   
+    // }
+
+}
+
+fn get_rectangles(bytes: &[u8], swidth: u16, sheight: u16) -> Vec<Rec> {
+    let side = 16 as u16;  
+    let xrects = swidth as u16 / side;
+    let rwidth = swidth as u16 % side;
+    let yrects = sheight as u16 / side;
+    let rheight = sheight as u16 % side;
+    let pwidth = side * xrects; // partial width without reminder
+    let pheight = side * yrects; // partial height without reminder
+    let mut rectangles = Vec::<Rec>::new();
     
     let mut buffer: Vec<u8> = Vec::new();
-    buffer.resize(lwidth as usize * lheight as usize * 3, 0);
-
-    let xrects = swidth as u16 / lwidth;
-    let rwidth = swidth as u16 % lwidth;
-    let yrects = sheight as u16 / lheight;
-    let rheight = sheight as u16 % lheight;
-    let pwidth = lwidth * xrects; // partial width without reminder
-    let pheight = lheight * yrects; // partial height without reminder
-    
-
+    buffer.resize(side as usize * side as usize * 3, 0);
 
     let mut n = 0;
-    for y in (0..pheight).step_by(lheight as usize) {
-        for x in (0..pwidth).step_by(lwidth as usize) {
-            println!("x={x},y={y}");
+    for y in (0..pheight).step_by(side as usize) {
+        for x in (0..pwidth).step_by(side as usize) {
+            // println!("x={x},y={y}");
             let mut index = 0;
-            for j in 0..lheight {
+            for j in 0..side {
                 let mut sindex = ((x as usize + ((y+j) as usize * swidth as usize)) * 3) as usize;
-                for _ in 0..lwidth {
-                    buffer[index] = bytes1[sindex];
-                    buffer[index+1] = bytes1[sindex+1];
-                    buffer[index+2] = bytes1[sindex+2];
+                for _ in 0..side {
+                    buffer[index] = bytes[sindex];
+                    buffer[index+1] = bytes[sindex+1];
+                    buffer[index+2] = bytes[sindex+2];
                     index += 3;
                     sindex += 3;
                 } 
@@ -66,31 +109,31 @@ fn main() {
                 index: n,
                 x: x as u16,
                 y: y as u16,
-                width: lwidth as u16,
-                height: lheight as u16,
+                width: side as u16,
+                height: side as u16,
                 bytes : buffer.clone(),
             };
             rectangles.push(rec);
 
             //let name = format!("C:\\Users\\san\\Pictures\\Screenshots\\Result_{n}.png");
-            //image::save_buffer(&name, &buffer, lwidth, lheight, image::ColorType::Rgb8).unwrap();
+            //image::save_buffer(&name, &buffer, side, side, image::ColorType::Rgb8).unwrap();
         }
         n += 1;
     }
 
     // reminder column
-    println!("rwidth: {rwidth}, rheight: {rheight}");
+    //println!("rwidth: {rwidth}, rheight: {rheight}");
     n = xrects+1;
-    buffer.resize(rwidth as usize * lheight as usize * 3, 0);
-    for y in (0..pheight).step_by(lheight as usize) {
+    buffer.resize(rwidth as usize * side as usize * 3, 0);
+    for y in (0..pheight).step_by(side as usize) {
         let mut index = 0;
-        for j in 0..lheight {
+        for j in 0..side {
             let mut sindex = ((pwidth as usize + ((y+j) as usize* swidth as usize)) * 3) as usize;
             for _ in 0..rwidth {
                 //println!("sindex={sindex},y={y},index={index}");
-                buffer[index] = bytes1[sindex];
-                buffer[index+1] = bytes1[sindex+1];
-                buffer[index+2] = bytes1[sindex+2];
+                buffer[index] = bytes[sindex];
+                buffer[index+1] = bytes[sindex+1];
+                buffer[index+2] = bytes[sindex+2];
                 index += 3;
                 sindex += 3;
             } 
@@ -100,26 +143,26 @@ fn main() {
             x: pwidth as u16,
             y: y as u16,
             width: rwidth as u16,
-            height: lheight as u16,
+            height: side as u16,
             bytes : buffer.clone(),
         };
         rectangles.push(rec);
         //let name = format!("C:\\Users\\san\\Pictures\\Screenshots\\Result_{n}.png");
         n += xrects+1;
-        //image::save_buffer(&name, &buffer, rwidth, lheight, image::ColorType::Rgb8).unwrap();
+        //image::save_buffer(&name, &buffer, rwidth, side, image::ColorType::Rgb8).unwrap();
     }
 
     // reminder row
     n = (xrects+1) * yrects +1;
-    buffer.resize(lwidth as usize * rheight as usize * 3, 0);
-    for x in (0..pwidth).step_by(lwidth as usize) {
+    buffer.resize(side as usize * rheight as usize * 3, 0);
+    for x in (0..pwidth).step_by(side as usize) {
         let mut index = 0;
         for j in 0..rheight {
             let mut sindex = ((x as usize+ ((pheight+j) as usize * swidth as usize)) * 3) as usize;
-            for _ in 0..lwidth {
-                buffer[index] = bytes1[sindex];
-                buffer[index+1] = bytes1[sindex+1];
-                buffer[index+2] = bytes1[sindex+2];
+            for _ in 0..side {
+                buffer[index] = bytes[sindex];
+                buffer[index+1] = bytes[sindex+1];
+                buffer[index+2] = bytes[sindex+2];
                 index += 3;
                 sindex += 3;
             } 
@@ -128,14 +171,14 @@ fn main() {
             index: n,
             x: x as u16,
             y: pheight as u16,
-            width: lwidth as u16,
+            width: side as u16,
             height: rheight as u16,
             bytes : buffer.clone(),
         };
         rectangles.push(rec);
         //let name = format!("C:\\Users\\san\\Pictures\\Screenshots\\Result_{n}.png");
         n += 1;
-        //image::save_buffer(&name, &buffer, lwidth, rheight, image::ColorType::Rgb8).unwrap();
+        //image::save_buffer(&name, &buffer, side, rheight, image::ColorType::Rgb8).unwrap();
     }
 
     // reminder last corner
@@ -144,9 +187,9 @@ fn main() {
     for j in 0..rheight {
         let mut sindex = ((pwidth as usize+ ((pheight+j) as usize* swidth as usize)) * 3) as usize;
         for _ in 0..rwidth {
-            buffer[index] = bytes1[sindex];
-            buffer[index+1] = bytes1[sindex+1];
-            buffer[index+2] = bytes1[sindex+2];
+            buffer[index] = bytes[sindex];
+            buffer[index+1] = bytes[sindex+1];
+            buffer[index+2] = bytes[sindex+2];
             index += 3;
             sindex += 3;
         } 
@@ -161,11 +204,5 @@ fn main() {
         bytes : buffer.clone(),
     };
     rectangles.push(rec);
-
-    // save rectangles
-    for r in rectangles.iter() {
-        let name = format!("C:\\Users\\san\\Pictures\\Screenshots\\Result_{}.png",r.index);
-        image::save_buffer(&name, &r.bytes, r.width as u32, r.height as u32, image::ColorType::Rgb8).unwrap();   
-    }
-
+    rectangles
 }
