c1f1318a20e64885d74d3accb760e383f9561e73
San <sganis@gmail.com>
2025-08-23 12:02:41 +0300

    Added server script

--- tunnel/server.sh
+++ tunnel/server.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 while true; do
-  echo -e "HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello" | nc -l 127.0.0.1 8080
+  	echo -e "HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello" | nc -l 127.0.0.1 9000
 done
-
